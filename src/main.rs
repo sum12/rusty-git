85cc2b65f749fa3ee714c686d00bf4f4026b772b
sum12 <sumitjami@gmail.com>
2020-04-11 16:32:57 +0200

    GitRepo struct and helper to get path into .git dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct GitRepo<'a> {
+    git_path: &'a str,
+    workdir_path: &'a str,
+}
+
+fn repo_path<'a>(r: &'a GitRepo, path: Vec<&str>) -> String {
+    let mut ret = r.git_path.clone().to_string();
+
+    for p in &path {
+        ret = format!("{}/{}", ret, p)
+    }
+    ret
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "repo_path = {}",
+        repo_path(
+            &GitRepo {
+                git_path: ".git",
+                workdir_path: "."
+            },
+            vec!["refs"]
+        )
+    )
 }
