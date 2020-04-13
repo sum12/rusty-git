809ab7321580973fe66725739b6a9f017fc52e80
sum12 <sumitjami@gmail.com>
2020-04-13 14:08:33 +0200

    use std::path for generating path

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
+use std::path::Path;
 struct GitRepo<'a> {
     git_path: &'a str,
     workdir_path: &'a str,
 }
 
-fn repo_path<'a>(r: &'a GitRepo, path: Vec<&str>) -> String {
-    let mut ret = r.git_path.clone().to_string();
-
+fn repo_path<'a>(r: &'a GitRepo, path: Vec<&str>) -> &'a Path {
+    let p = Path::new(r.git_path);
+    let mut ret = p.to_path_buf();
     for p in &path {
-        ret = format!("{}/{}", ret, p)
+        ret.push(Path::new(p));
     }
-    ret
+    p
 }
 
 fn main() {
-    println!(
-        "repo_path = {}",
-        repo_path(
-            &GitRepo {
-                git_path: ".git",
-                workdir_path: "."
-            },
-            vec!["refs"]
-        )
-    )
+    let r = &GitRepo {
+        git_path: ".git",
+        workdir_path: ".",
+    };
+
+    let s = repo_path(r, vec!["refs"]).to_str();
+    //println!("{:?}", s);
+
+    if let Some(p) = s {
+        println!("{}", p);
+    }
 }
