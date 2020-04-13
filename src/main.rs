3148eea42fb24d5775ee63d17e2ea6ce83569184
sum12 <sumitjami@gmail.com>
2020-04-13 16:12:26 +0200

    repo_file and repo_dir implemented.
    this implementation uses pathbuf extensively. Path cannot be used
    because it is unsized type which cannot be passed around.
    
    There is no owner for PathBuf therefore its owner is passed around. But
    even if it is passed around as String instead. each function will end up
    recreateing Path/PathBuf from the String.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,51 @@
-use std::path::Path;
+use std::fs;
+use std::path::PathBuf;
 struct GitRepo<'a> {
     git_path: &'a str,
-    workdir_path: &'a str,
+    //workdir_path: &'a str,
 }
 
-fn repo_path<'a>(r: &'a GitRepo, path: Vec<&str>) -> &'a Path {
-    let p = Path::new(r.git_path);
-    let mut ret = p.to_path_buf();
-    for p in &path {
-        ret.push(Path::new(p));
+fn repo_path(r: &GitRepo, path: &[&str]) -> PathBuf {
+    let mut ret = PathBuf::from(r.git_path);
+    for p in path {
+        ret.push(p);
     }
-    p
+    ret
+}
+
+fn repo_dir(r: &GitRepo, path: &[&str], mkdir: bool) -> Option<PathBuf> {
+    let rp = repo_path(r, path);
+
+    if rp.exists() && rp.is_dir() {
+        return Some(rp);
+    }
+    if mkdir {
+        if let Some(rd) = rp.to_str() {
+            fs::create_dir_all(rd).expect(&format!("create dir failed: {}", rd));
+            return Some(rp);
+        }
+    }
+    return None;
+}
+
+fn repo_file(r: &GitRepo, path: &[&str], mkdir: bool) -> Option<PathBuf> {
+    if let Some((_, dir_path)) = path.split_last() {
+        if let Some(_) = repo_dir(r, &dir_path.to_vec(), mkdir) {
+            return Some(repo_path(r, path));
+        }
+    }
+    return None;
 }
 
 fn main() {
     let r = &GitRepo {
         git_path: ".git",
-        workdir_path: ".",
+        //workdir_path: ".",
     };
-
-    let s = repo_path(r, vec!["refs"]).to_str();
-    //println!("{:?}", s);
+    let v = vec!["refs", "remotes", "origins", "HEAD"];
+    let s = repo_file(r, &v, true);
 
     if let Some(p) = s {
-        println!("{}", p);
+        println!("this is = {:?}", p);
     }
 }
